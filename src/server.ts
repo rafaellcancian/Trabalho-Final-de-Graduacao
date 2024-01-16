588ff0df41a64abe16e083213e189d891c05ba3b
Rafael Cancian <rafaellonderocancian@gmail.com>
2024-01-16 17:35:07 -0300

    fix: Alteração do DNS e porta da aplicação

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ import { app } from './app';
 
 /** Servidor */
 const server = http.createServer(app);
-const port = 8080;
+const port = 3000;
 server.listen(port, '0.0.0.0', () => console.log('O servidor está em execução na porta: ' + port));
