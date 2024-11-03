3059b3ec352c5fdae3e604d1d12038a51c3175bf
Rafael Cancian <rafaellonderocancian@gmail.com>
2024-11-03 14:32:16 -0300

    fix: Removido valores padrão do environment para disparar as mensagens de warn

--- src/util/environment.ts
+++ src/util/environment.ts
@@ -5,11 +5,11 @@ dotenv.config({
 });
 
 // DBConfig
-const pgHost = process.env.PG_HOST || 'localhost';
+const pgHost = process.env.PG_HOST;
 const pgPort = parseInt(process.env.PG_PORT || '5432');
-const pgUser = process.env.PG_USER || 'postgres';
-const pgPassword = process.env.PG_PASSWORD || 'password';
-const pgDatabase = process.env.PG_DATABASE || 'postgres';
+const pgUser = process.env.PG_USER;
+const pgPassword = process.env.PG_PASSWORD;
+const pgDatabase = process.env.PG_DATABASE;
 
 // Invertexto API
 const invertextoApiToken = process.env.INVERTEXTO_API_TOKEN;
@@ -23,14 +23,14 @@ console.log({
   invertextoApiToken
 });
 
-if (!invertextoApiToken) {
-  console.warn('[WARN] Não foi possível encontrar a variável de ambiente "invertextoApiToken".');
-  console.warn('[WARN] O provedor Invertexto não funcionará sem um token especificado no .env.production, pois se trata de uma API privada que requer uma conta.');
-}
-
 if (!pgHost || !pgUser || !pgPassword || !pgDatabase) {
   console.warn('[WARN] Não foi possível encontrar uma ou mais variáveis de ambiente do banco de dados PostgreSQL.');
   console.warn('[WARN] A aplicação executará sem um banco de dados PostgreSQL normalmente, porém não fará os registros das requisições. Para configurar o banco de dados localmente, ver o arquivo .env.example.');
+}
+
+if (!invertextoApiToken) {
+  console.warn('[WARN] Não foi possível encontrar a variável de ambiente "invertextoApiToken".');
+  console.warn('[WARN] O provedor Invertexto não funcionará sem um token especificado no .env.production, pois se trata de uma API privada que requer uma conta.');
 }
 
 export {
